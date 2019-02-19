3b5c1dd419a4e68f3090576b5bdc467c3d930a79
Kieran Scott <kieranscott1985@gmail.com>
2019-02-19 14:19:33 +0000

    2nd commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    'mongodb://kieran2:DAfYBXOKNIggC9R3@cluster0-shard-00-00-vitri.mongodb.net:27017,cluster0-shard-00-01-vitri.mongodb.net:27017,cluster0-shard-00-02-vitri.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+};
